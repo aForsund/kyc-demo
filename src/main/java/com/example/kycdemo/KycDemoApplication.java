e51448f3b79214d9442831f920aefe095e77120a
Alexander Førsund <alexanderforsund@gmail.com>
2021-11-20 22:04:04 +0100

    first commit

---
+++ src/main/java/com/example/kycdemo/KycDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kycdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycDemoApplication.class, args);
+	}
+
+}
