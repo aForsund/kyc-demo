9c36fc41a1f8c44817593ed4b5f1195a06dd9fc0
Alexander Førsund <alexanderforsund@gmail.com>
2021-11-22 19:38:46 +0100

    deployed test to azure

--- src/main/java/com/example/kycdemo/model/Person.java
+++ src/main/java/com/example/kycdemo/model/Person.java
@@ -2,4 +2,14 @@ package com.example.kycdemo.model;
 
 public class Person {
 
+    private String name;
+    public Person() {}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
